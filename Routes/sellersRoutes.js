17b8e10f761432acefbbd699f1b23e664a06b702
Felix Asare <flexywork327@gmail.com>
2023-06-22 04:58:06 +0000

    added the function to post images of product to cloudinary

--- Routes/sellersRoutes.js
+++ Routes/sellersRoutes.js
@@ -18,8 +18,8 @@ router.get("/me/:id", protect, getUser);
 // post Routes
 router.post("/login", loginSeller);
 router.post("/register", registerSeller);
-router.post("/reset_password", resetPassword);
 router.post("/activate_user", activateSeller);
+router.post("/reset_password", resetPassword);
 router.post("/update/:id", protect, updateSeller);
 router.post("/forgot_password", protect, forgotPassword);
 router.post("/change_password/:id", protect, changePassword);
