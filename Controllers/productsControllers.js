96d5f513ccf789a10fca43c1a617346de5ddb3ab
Felix Asare <flexywork327@gmail.com>
2023-07-08 03:04:02 +0000

    added useful comments to the products controller file

--- Controllers/productsControllers.js
+++ Controllers/productsControllers.js
@@ -3,8 +3,8 @@ const cloudinary = require("../Utils/cloudinary");
 
 //TODO: ======================================================== Get All Product ========================================================
 
-// desc Get Product Details
-// @route post /get_product_detail
+// desc Get All Product
+// @route get /products/get_all_products
 // @access public
 const get_all_Products = async (req, res) => {
   try {
@@ -25,12 +25,11 @@ const get_all_Products = async (req, res) => {
 
 //TODO: ======================================================== Get All Active Product ========================================================
 
-// desc Get Product Details
-// @route post /get_product_detail
+// desc Get All Active Product
+// @route get /products/active
 // @access public
 const activeProduct = async (req, res) => {
   try {
-    //  get the product from the database
     const product = await pool.query(
       "SELECT * FROM products WHERE product_activated = true"
     );
@@ -49,12 +48,11 @@ const activeProduct = async (req, res) => {
 
 //TODO: ======================================================== Get All Inactive Product ========================================================
 
-// desc Get Product Details
-// @route post /get_product_detail
+// desc Get All Inactive Product
+// @route get /products/inactive
 // @access public
 const inactiveProduct = async (req, res) => {
   try {
-    //  get the product from the database
     const product = await pool.query(
       "SELECT * FROM products WHERE product_activated = false"
     );
@@ -74,12 +72,11 @@ const inactiveProduct = async (req, res) => {
 //TODO: ======================================================== Get Product Details ========================================================
 
 // desc Get Product Details
-// @route post /get_product_detail
+// @route post /products/get_product_detail
 // @access public
 const get_Product_Details = async (req, res) => {
   const { post_id } = req.body;
   try {
-    //  get the product from the database
     const product = await pool.query("SELECT * FROM products WHERE id = $1", [
       post_id,
     ]);
@@ -105,11 +102,10 @@ const get_Product_Details = async (req, res) => {
 
 //TODO: =================================================================== List Product Category ===================================================================
 
-//GET /list_product_category
 //@desc  List Product Category
+//GET /products/product_category
 //@access public
 const list_Product_Category = async (req, res) => {
-  // find PRODUCT CATEGORY from the database
   try {
     const product_category = await pool.query("SELECT * FROM product_category");
     res.json({
@@ -127,8 +123,8 @@ const list_Product_Category = async (req, res) => {
 // TODO: ======================================================== Post Product ========================================================
 
 // desc Post Product
-// @route post /post_product
-// @access public
+// @route post /products/post_product
+// @access private (need to login)
 const post_Product = async (req, res) => {
   const {
     product_name,
@@ -147,7 +143,6 @@ const post_Product = async (req, res) => {
       folder: "Justlink",
     });
 
-    //  get the product from the database
     const product = await pool.query(
       "INSERT INTO products (product_name, product_category, product_description, product_price, product_image, product_quantity, product_section,seller_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
       [
@@ -176,11 +171,10 @@ const post_Product = async (req, res) => {
 
 // TODO: ======================================================== Edit Product ========================================================
 
-// desc Post Product
-// @route post /post_product
-// @access public
+// desc Edit Product
+// @route post /products/edit_product
+// @access private (need to login)
 const editProduct = async (req, res) => {
-  // Edit Product
   const {
     product_name,
     product_category,
@@ -228,13 +222,12 @@ const editProduct = async (req, res) => {
 };
 
 // TODO: ======================================================== Get Product By Category ========================================================
-// desc Get Product Details
-// @route post /get_product_detail
+// desc Get Product By Category
+// @route post /products/get_product_by_category
 // @access public
 const get_Product_By_Category = async (req, res) => {
   const { product_section } = req.body;
   try {
-    //  get the product from the database
     const product = await pool.query(
       "SELECT * FROM products WHERE product_section = $1 AND product_activated = true",
       [product_section]
@@ -243,7 +236,7 @@ const get_Product_By_Category = async (req, res) => {
     if (product.rows.length === 0) {
       return res.json({
         status: 404,
-        message: "Product not found",
+        message: "Product(s) not found",
       });
     } else if (product.rows.length > 0) {
       res.json({
@@ -260,22 +253,22 @@ const get_Product_By_Category = async (req, res) => {
 };
 
 // TODO: ======================================================== Search Product ========================================================
-// desc Get Product Details
-// @route post /get_product_detail
+// desc Search Product By Name
+// @route post /products/search_product
 // @access public
 const search_Product = async (req, res) => {
   const { product_name } = req.body;
   try {
-    //  get the product from the database
+    //  get the product from the database irrespective of the case of the product_name
     const product = await pool.query(
-      "SELECT * FROM products WHERE product_name LIKE $1 AND product_activated = true",
+      "SELECT * FROM products WHERE LOWER(product_name) LIKE LOWER($1) AND product_activated = true",
       ["%" + product_name + "%"]
     );
 
     if (product.rows.length === 0) {
       return res.json({
         status: 404,
-        message: "Product not found",
+        message: "Product(s) not found",
       });
     } else if (product.rows.length > 0) {
       res.json({
@@ -292,13 +285,12 @@ const search_Product = async (req, res) => {
 };
 
 // TODO: ======================================================== Add To Cart ========================================================
-// desc Get Product Details
-// @route post /get_product_detail
-// @access public
+// desc Add Product To Cart
+// @route post /products/add_to_cart
+// @access private (need to login)
 const addToCart = async (req, res) => {
   const { product_id, cart_quantity } = req.body;
   try {
-    //  get the product from the database
     const product = await pool.query("SELECT * FROM products WHERE id = $1", [
       product_id,
     ]);
@@ -309,6 +301,7 @@ const addToCart = async (req, res) => {
         message: "Product not found",
       });
     } else if (product.rows.length > 0) {
+      // extract all the needed data from the product pulled from the database
       const {
         product_name,
         product_category,
@@ -321,6 +314,7 @@ const addToCart = async (req, res) => {
         seller_id,
       } = product.rows[0];
 
+      //  add the product to the cart
       const add_Product = await pool.query(
         "INSERT INTO cart (product_name, product_category, product_description, product_price, product_quantity,product_image, product_section,seller_id,product_activated,cart_quantity,shopper_id,total_cost) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *",
         [
@@ -335,7 +329,7 @@ const addToCart = async (req, res) => {
           product_activated,
           cart_quantity,
           req.user.id,
-          (total_cost = product_price * cart_quantity),
+          Number((total_cost = product_price * cart_quantity)),
         ]
       );
 
@@ -352,13 +346,34 @@ const addToCart = async (req, res) => {
   }
 };
 
+// TODO: ======================================================== Remove From Cart ========================================================
+// desc Remove Product From Cart
+// @route post /products/remove_from_cart
+// @access private (need to login)
+const removeFromCart = async (req, res) => {
+  const { cart_id } = req.body;
+  try {
+    const product = await pool.query("DELETE FROM cart WHERE cart_id = $1", [
+      cart_id,
+    ]);
+
+    res.json({
+      status: 200,
+      message: "Product removed from cart successfully",
+    });
+  } catch (error) {
+    res.json({
+      message: `${error}`,
+    });
+  }
+};
+
 // TODO: ======================================================== Get User Cart Items ========================================================
-// desc Get Product Details
-// @route post /get_product_detail
-// @access public
+// desc Get Login User Products in Cart
+// @route get /products/get_user_cart_item
+// @access private (need to login)
 const getUserCartItems = async (req, res) => {
   try {
-    //  get the product from the database
     const product = await pool.query(
       "SELECT * FROM cart WHERE shopper_id = $1",
       [req.user.id]
@@ -367,10 +382,10 @@ const getUserCartItems = async (req, res) => {
     if (product.rows.length === 0) {
       return res.json({
         status: 404,
-        message: "Product not found",
+        message: "Product(s) not found",
       });
     } else if (product.rows.length > 0) {
-      // loop through all the products in the cart and add the total cost as cart_total
+      // loop through all the products in the cart and add the total_cost as cart_total
       let cart_total = 0;
       product.rows.forEach((item) => {
         // convert the total_cost to a number
@@ -388,29 +403,6 @@ const getUserCartItems = async (req, res) => {
         count: product.rows.length,
       });
     }
-  } catch (error) {
-    res.json({
-      message: `${error}`,
-    });
-  }
-};
-
-// TODO: ======================================================== Remove From Cart ========================================================
-// desc Get Product Details
-// @route post /get_product_detail
-// @access public
-const removeFromCart = async (req, res) => {
-  const { cart_id } = req.body;
-  try {
-    //  get the product from the database
-    const product = await pool.query("DELETE FROM cart WHERE cart_id = $1", [
-      cart_id,
-    ]);
-
-    res.json({
-      status: 200,
-      message: "Product removed from cart successfully",
-    });
   } catch (error) {
     res.json({
       message: `${error}`,
